0379cc4a9b04b5d67153a07d3c880e41f9842e56
Hunter <hunterbuckshot@aol.com>
2018-03-30 12:15:00 -0500

    login with amazon is working

--- meal_plan/app.js
+++ meal_plan/app.js
@@ -3,24 +3,127 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var passport = require('passport');
+var util = require('util');
+var AmazonStrategy = require('passport-amazon').Strategy;
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var methodOverride = require('method-override')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var login = require('./routes/login');
+
+var AMAZON_CLIENT_ID = "amzn1.application-oa2-client.c84a394dab3c4fea9d228b3881caf672"
+var AMAZON_CLIENT_SECRET = "7f09ba172422925cc657d73fb59bcd1384a22a6f74190bd8cefec554a7fea5f4";
+
+// Passport session setup.
+//   To support persistent login sessions, Passport needs to be able to
+//   serialize users into and deserialize users out of the session.  Typically,
+//   this will be as simple as storing the user ID when serializing, and finding
+//   the user by ID when deserializing.  However, since this example does not
+//   have a database of user records, the complete Amazon profile is
+//   serialized and deserialized.
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+
+// Use the AmazonStrategy within Passport.
+//   Strategies in Passport require a `verify` function, which accept
+//   credentials (in this case, an accessToken, refreshToken, and Amazon
+//   profile), and invoke a callback with a user object.
+passport.use(new AmazonStrategy({
+    clientID: AMAZON_CLIENT_ID,
+    clientSecret: AMAZON_CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/amazon/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    // asynchronous verification, for effect...
+    process.nextTick(function () {
+      
+      // To keep the example simple, the user's Amazon profile is returned to
+      // represent the logged-in user.  In a typical application, you would want
+      // to associate the Amazon account with a user record in your database,
+      // and return that user instead.
+      return done(null, profile);
+    });
+  }
+));
 
 var app = express();
+console.log('listening on port 3000')
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cookieParser());
+app.use(bodyParser());
+app.use(methodOverride());
+app.use(session({ secret: 'keyboard cat' }));
+// Initialize Passport!  Also use passport.session() middleware, to support
+// persistent login sessions (recommended).
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/login', login);
+
+app.get('/', function(req, res){
+  res.render('index', { user: req.user });
+});
+
+app.get('/account', ensureAuthenticated, function(req, res){
+  res.render('account', { user: req.user });
+});
+
+app.get('/login', function(req, res){
+  res.render('login', { user: req.user });
+});
+
+// GET /auth/amazon
+//   Use passport.authenticate() as route middleware to authenticate the
+//   request.  The first step in Amazon authentication will involve
+//   redirecting the user to amazon.com.  After authorization, Amazon
+//   will redirect the user back to this application at /auth/amazon/callback
+app.get('/auth/amazon',
+  passport.authenticate('amazon', { scope: ['profile', 'postal_code'] }),
+  function(req, res){
+    // The request will be redirected to Amazon for authentication, so this
+    // function will not be called.
+  });
+
+// GET /auth/amazon/callback
+//   Use passport.authenticate() as route middleware to authenticate the
+//   request.  If authentication fails, the user will be redirected back to the
+//   login page.  Otherwise, the primary route function function will be called,
+//   which, in this example, will redirect the user to the home page.
+app.get('/auth/amazon/callback', 
+  passport.authenticate('amazon', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  });
+
+app.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
+});
+// Simple route middleware to ensure user is authenticated.
+//   Use this route middleware on any resource that needs to be protected.  If
+//   the request is authenticated (typically via a persistent login session),
+//   the request will proceed.  Otherwise, the user will be redirected to the
+//   login page.
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) { return next(); }
+  res.redirect('/login')
+}
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

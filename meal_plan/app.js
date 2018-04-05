6e20752c3b4596441043d70fad6dd7be9007c56e
Zak <adivinepk@gmail.com>
2018-04-05 00:42:10 -0500

    Added the survey to sync with db and added pages for edit user & meal

--- meal_plan/app.js
+++ meal_plan/app.js
@@ -104,6 +104,47 @@ app.get('/test', function(req, res){
   res.render('test', {});
 });
 
+app.get('/view-meal-plan', function(req, res)
+{
+res.render('view-meal-plan', {});
+});
+
+app.get('/edit-user-info', function(req, res)
+{
+res.render('userinfo', {user: req.user})
+});
+
+app.post('/submitsurvey', function(req, res)
+{
+  db.getConnection(function(err, mclient) {
+   mclient.query('INSERT INTO userData(UserID, gender, height, weight, age, activityLevel, allergies) VALUES ("'+req.user.id+'", "'+req.body.gender+'", "'+req.body.height+'", "'+req.body.weight+'", "'+req.body.age+'", "'+req.body.activityLevel+'", "'+req.body.allergies+'")', function (err, rows, fields)
+  {
+       mclient.release();
+      
+     if (err) throw err;
+
+     
+    console.log("Added survey information for: "+req.user.id);
+
+    });
+
+});
+db.getConnection(function(err, mclient) {
+  mclient.query('Update amazonAuth SET new = 1 WHERE id="'+req.user.id+'"', function (err, rows, fields)
+ {
+      mclient.release();
+     
+    if (err) throw err;
+
+    
+   console.log("Updated "+req.user.id+" to old member");
+   res.redirect('/');
+   });
+
+});
+
+})
+
 
 // GET /auth/amazon
 //   Use passport.authenticate() as route middleware to authenticate the
--- meal_plan/routes/Survey.js
+++ meal_plan/routes/Survey.js
@@ -1,4 +1,4 @@
-import React from 'react';
+// import React from 'react';
 //ENTRANCE SURVEY
 //By Dylan Campbell
 

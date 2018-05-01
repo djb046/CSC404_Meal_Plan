d3957e4087984963a06c3ed485550c94c2fab09b
carmkarm <carmnich@aol.com>
2018-05-01 00:03:49 -0500

    Update login.jsx

--- meal_plan/jsx/login.jsx
+++ meal_plan/jsx/login.jsx
@@ -1,21 +1,59 @@
-import BackgroundSlideshow from 'react-background-slideshow'
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
-
+import { Container, Grid, Card, Icon, Image, Menu, Progress, Item, Modal, Button, Header } from 'semantic-ui-react';
+	
 
 class Login extends React.Component {
-
   render() {
-    return (
-      <div align="center">
-        <h2>Welcome! Please log in.</h2>
 
-        <a href="/auth/amazon">  <img border="0" alt="Login with Amazon" src="https://images-na.ssl-images-amazon.com/images/G/01/lwa/btnLWA_gold_156x32.png" width="156" height="32" /></a>
-        
-        {/* <BackgroundSlideshow images={[mater, picnic, rav, tater]} /> */}
+  return (
+  <div>
+      <br>
+      </br>
+      <br>
+      </br>
+      <div align="center">
+      <h1>Smart Meal Plan</h1>
+      <br>
+      </br>
+      <br>
+      </br>
+ 
+    <Container className="dashPanel4">
+      <div  align="center">
+      <br>
+      </br>
+      <br>
+      </br>
+      <br>
+      </br>
+      <br>
+      </br>
+      <br>
+      </br>
+      <br>
+      </br>
+      <br>
+      </br>
+      <br>
+      </br>
+      <Container className="dashPanel3">
+      <br>
+        </br>
+        <br>
+        </br>
+     
+        <h2>Welcome! Please log in.</h2>
+        <br>
+        </br>
+
+        <a href="/auth/amazon">  <img border="0" alt="Login with Amazon" src="https://images-na.ssl-images-amazon.com/images/G/01/lwa/btnLWA_gold_156x32.png" width="156" height="32" /></a> 
+        </Container>
       </div>
-    )
+       </Container>
+       </div>
+       </div>
+       )
   }
 }
 
